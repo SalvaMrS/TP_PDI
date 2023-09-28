da8f305cf17bbc79ea0f02541d9e8a47dec23589
SalvaMrS <101283370+SalvaMrS@users.noreply.github.com>
2023-09-28 17:29:06 -0300

    Actividad 2 completada

--- Actividad_2.py
+++ Actividad_2.py
@@ -22,9 +22,9 @@ for i, promedio in enumerate(promedios_filas):
         coordenadas_lineas.append(i)
         
         if len(coordenadas_lineas) > 1:
-            # Recortar la imagen en áreas definidas por las líneas
-            inicio = coordenadas_lineas[-2] + 1  # Añadir 1 para evitar la línea anterior
-            fin = coordenadas_lineas[-1] - 1     # Restar 1 para evitar la línea siguiente
+            # Recortar la imagen en áreas definidas por las líneas, ajustando las coordenadas
+            inicio = coordenadas_lineas[-2] + 2  # Añadir 2 para evitar las 2 líneas anteriores
+            fin = coordenadas_lineas[-1] - 2     # Restar 2 para evitar las 2 líneas siguientes
             seccion = imagen[inicio:fin, :]
             recortes.append(seccion)
 
@@ -46,13 +46,87 @@ for recorte in recortes:
     
     # Conservar la parte entre la segunda línea divisoria y la última
     if len(coordenadas_lineas_columnas) >= 2:
-        inicio = coordenadas_lineas_columnas[1] + 1  # Añadir 1 para evitar la línea anterior
-        fin = coordenadas_lineas_columnas[-1] - 1    # Restar 1 para evitar la línea siguiente
+        inicio = coordenadas_lineas_columnas[1] + 2  # Añadir 1 para evitar la línea anterior
+        fin = coordenadas_lineas_columnas[-1] - 2    # Restar 1 para evitar la línea siguiente
         recorte_conservado = recorte[:, inicio:fin]
         recortes_conservados.append(recorte_conservado)
 
-# Mostrar los recortes conservados
+def count_caracter(recorte):
+    # Binarizar el recorte (cambiar umbral si es necesario)
+    _, binarizado = cv2.threshold(recorte, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Etiquetar componentes conectados en el recorte
+    num_etiquetas, _, _, _ = cv2.connectedComponentsWithStats(binarizado, connectivity=4, ltype=cv2.CV_32S)
+
+    # Restar 1 para excluir la etiqueta 0 (fondo)
+    num_caracteres = num_etiquetas - 1
+
+    return num_caracteres
+
+def count_word(recorte):
+    # Binarizar el recorte (cambiar umbral si es necesario)
+    _, binarizado = cv2.threshold(recorte, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+
+    # Encontrar los contornos de los caracteres
+    contornos, _ = cv2.findContours(binarizado, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    distancia_umbral = 5  # Umbral de distancia entre caracteres para considerar una palabra
+    num_palabras = 0
+
+    if len(contornos) > 0:
+        # Ordenar los contornos de izquierda a derecha
+        contornos = sorted(contornos, key=lambda x: cv2.boundingRect(x)[0])
+
+        # Calcular la distancia entre el borde derecho de un caracter y el borde izquierdo del siguiente
+        for i in range(len(contornos) - 1):
+            x1, _, w1, _ = cv2.boundingRect(contornos[i])
+            x2, _, _, _ = cv2.boundingRect(contornos[i + 1])
+            distancia_entre_caracteres = x2 - (x1 + w1)
+
+            # Si la distancia es mayor que el umbral, considerarlo como una nueva palabra
+            if distancia_entre_caracteres > distancia_umbral:
+                num_palabras += 1
+
+        # Contar la última palabra (si la hubiera)
+        num_palabras += 1
+
+    return num_palabras
+
+# Nombres de los campos correspondientes a cada recorte
+nombres_campos = [
+    "Nombre",
+    "Edad",
+    "Mail",
+    "Legajo",
+    "Pregunta 1",
+    "Pregunta 2",
+    "Pregunta 3",
+    "Comentarios"
+]
+
+# Crear una lista para almacenar los resultados
+resultados = []
+
+# Verificar las condiciones para cada recorte
 for idx, recorte in enumerate(recortes_conservados, 1):
-    plt.imshow(recorte, cmap='gray')
-    plt.title(f"Recorte {idx} - Parte conservada")
-    plt.show()
+    num_caracteres = count_caracter(recorte)
+    num_palabras = count_word(recorte)
+
+    if idx == 1 and 2 >= num_palabras <= 25:
+        resultados.append("OK")
+    elif idx == 2 and num_caracteres in (2, 3):
+        resultados.append("OK")
+    elif idx == 3 and num_palabras == 1 and num_caracteres <= 25:
+        resultados.append("OK")
+    elif idx == 4 and num_caracteres == 8 and num_palabras == 1:
+        resultados.append("OK")
+    elif 5 <= idx <= 7 and num_caracteres == 2:
+        resultados.append("OK")
+    elif idx == 8 and num_caracteres <= 25:
+        resultados.append("OK")
+    else:
+        resultados.append("MAL")
+
+# Imprimir los resultados en el formato especificado
+for idx, resultado in enumerate(resultados, 1):
+    print(f"{idx}. {nombres_campos[idx-1]}:\t{resultado}")
