0414c42d8d6104efa25b2d68e93572d94dcaac17
pepe <dani_rc_22@hotmail.com>
2023-09-29 10:42:02 -0300

    version 1.3

--- localhistogramequalization/main.py
+++ localhistogramequalization/main.py
@@ -21,49 +21,100 @@ def setup_logger():
 
 
 class LocalHistogramEqualizer:
+    """
+    Clase para realizar la ecualización local del histograma en una imagen en escala de grises.
+
+    Args:
+       image_path (str): Ruta de la imagen de entrada.
+       window_size (int): Tamaño de la ventana para la ecualización local.
+
+    Attributes:
+       image (numpy.ndarray): La imagen en escala de grises a procesar.
+       window_size (int): El tamaño de la ventana para la ecualización local.
+    """
+
     def __init__(self, image_path, window_size):
         self.image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
         self.window_size = window_size
         self.logger = setup_logger()
 
     def equalize(self):
+        """
+        Realiza la ecualización local del histograma en la imagen de entrada.
+        Returns:
+           numpy.ndarray: La imagen procesada con ecualización local del histograma, o None si ocurre un error.
+        """
         try:
             height, width = self.image.shape[:2]
             half_window = self.window_size // 2
             output_image = np.copy(self.image)
 
+            # Calcular histograma y CDF una vez para toda la imagen
+            hist, _ = np.histogram(self.image.flatten(), bins=256, range=(0, 256))
+            cdf = hist.cumsum()
+            cdf_normalized = (cdf - cdf.min()) * 255 / (cdf.max() - cdf.min())
+
+            # Crear una matriz de índices para acceder a los valores de CDF normalizado
+            index_matrix = np.arange(256)
+
             for y in range(half_window, height - half_window):
                 for x in range(half_window, width - half_window):
                     roi = self.image[y - half_window:y + half_window + 1, x - half_window:x + half_window + 1]
-                    hist, _ = np.histogram(roi.flatten(), bins=256, range=(0, 256))
-                    if hist.max() == 0:
+                    if roi.size == 0:
                         continue
-                    cdf = hist.cumsum()
-                    if (cdf.max() - cdf.min()) == 0:
-                        continue
-                    cdf_normalized = (cdf - cdf.min()) * 255 / (cdf.max() - cdf.min())
-                    output_image[y, x] = cdf_normalized[roi[0, 0]]
+                    roi_flat = roi.flatten()
+                    cdf_values = cdf_normalized[roi_flat]
+                    mapped_values = np.interp(cdf_values, index_matrix, cdf_normalized)
+                    output_image[y, x] = mapped_values[0]
 
             return output_image
         except Exception as e:
-            self.logger.error(f'Error en la ecualización local del histograma: {str(e)}')
+            error_message = f'Error en la ecualización local del histograma: {str(e)}'
+            self.logger.error(error_message)
             return None
 
     def show_input_image(self):
+        """
+        Muestra la imagen de entrada en una ventana.
+
+        Raises:
+            Exception: Si ocurre un error al mostrar la imagen.
+        """
         try:
             cv2.imshow('Imagen de Entrada', self.image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
         except Exception as e:
-            self.logger.error(f'Error al mostrar la imagen de entrada: {str(e)}')
+            error_message = f'Error al mostrar la imagen de entrada: {str(e)}'
+            self.logger.error(error_message)
 
     def show_output_image(self, output_image):
+        """
+        Muestra la imagen procesada en una ventana.
+
+        Args:
+            output_image (numpy.ndarray): La imagen procesada.
+
+        Raises:
+            Exception: Si ocurre un error al mostrar la imagen.
+        """
         try:
             cv2.imshow('Imagen Procesada', output_image)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
         except Exception as e:
-            self.logger.error(f'Error al mostrar la imagen procesada: {str(e)}')
+            error_message = f'Error al mostrar la imagen procesada: {str(e)}'
+            self.logger.error(error_message)
 
     def close(self):
-        cv2.destroyAllWindows()
+        """
+        Cierra todas las ventanas abiertas.
+
+        Raises:
+            Exception: Si ocurre un error al cerrar las ventanas.
+        """
+        try:
+            cv2.destroyAllWindows()
+        except Exception as e:
+            error_message = f'Error al cerrar todas las ventanas: {str(e)}'
+            self.logger.error(error_message)
